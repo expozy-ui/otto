6c3072e4e0c81212359a128a39cd5f16971c9291
Expozy UI <167416126+expozy-ui@users.noreply.github.com>
2024-06-13 09:41:19 +0300

    Update class.page.php

--- core/classes/class.page.php
+++ core/classes/class.page.php
@@ -2,7 +2,7 @@
 if (!defined("_VALID_PHP")) { die('Direct access to this location is not allowed.'); }
 
 /** =========================================================
- * Class Page
+ * Class Page1
  * ========================================================== */
 class Page
 {
